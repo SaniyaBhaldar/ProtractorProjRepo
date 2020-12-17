7b21c9ae8fb21f63f1c9e07a099d364e6c84720d
SaniyaBhaldar <saniya.bhaldar96@gmail.com>
2020-12-17 21:00:39 +0530

    added sub test

--- tests/calculator.js
+++ tests/calculator.js
@@ -15,5 +15,22 @@ describe('calculator simple demo', function(){
         browser.sleep(2000);
     });
 
+    it('Subtraction Test',function(){
+        browser.get('http://juliemr.github.io/protractor-demo/');
+        //var input = element(by.model('first'));
+        //input.sendKeys('10');
+        element(by.model('first')).sendKeys(30);
+        element(by.xpath("/html/body/div/div/form/select/option[5]")).click();
+        element(by.model('second')).sendKeys(15);
+
+        element(by.css('[ng-click="doAddition()"]')).click();
+
+        //let result = element(by.cssContainingText('.ng-binding', '15'));
+        let result =element(by.xpath("/html/body/div/table/tbody/tr/td[3]")).getText();
+        expect(result.getText()).toEqual('5');
+
+        browser.sleep(2000);
+    });
+
    
 });
