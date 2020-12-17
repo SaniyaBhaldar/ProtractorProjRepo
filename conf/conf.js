ed0c74fe6def2f9f6fa371406217ac1b36a04faf
SaniyaBhaldar <saniya.bhaldar96@gmail.com>
2020-12-17 21:05:25 +0530

    commited changes related to allure reports

--- conf/conf.js
+++ conf/conf.js
@@ -12,10 +12,33 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['../tests/PetClinic.js'],
+  specs: ['../tests/calculator.js'
+],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
-  }
-};
+  },
+
+  onPrepare: function() {
+
+ 
+    browser.ignoreSynchronization = false;
+    browser.driver.manage().window().maximize();
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+  
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
+  
+}
+
+}
